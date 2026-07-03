9cbdf173227299e984d6d7c4f4ad93fcd4018086
zizo <zineb@nadak.ai>
2026-07-03 08:51:41 +0300

    fix: cache the answer_question() pipeline outputs to a JSON file, so RAGAS reads from that file instead of re-running the pipeline every time.

---
+++ eval/generate_answers.py
@@ -0,0 +1,39 @@
+# This file is made so that you dont have to rerun the whole pipeline (answer_question() call = scrape + embed + retrieve + Groq LLM call) for each eval testing. Use the answers stored in eval/pipeline_outputs.json
+
+'''
+#ingest your article and make sure it is in Chromadb
+from pipeline.ingestion import ingest_article
+print(ingest_article("https://en.wikipedia.org/wiki/Madrid"))
+'''
+
+import json 
+from dotenv import load_dotenv
+from pipeline.rag import answer_question
+import time # because of groqs rate limits. Spreads token usage over time so it does not hit the per-minute limit + n_resutls in retrieve to 3 instead of 5
+
+load_dotenv()
+
+golden_set_file = "golden_set_2"   # the golden set file inside eval/ folder without the ".json"
+
+with open (f"eval/{golden_set_file}.json", "r") as file:
+    golden_set = json.load(file)
+    
+samples_list = []
+
+for entry in golden_set:
+    llm_answer = answer_question(entry["question"], entry["source_article"]) # anser_question() returns a dict with 3 chunks (n_results) - {"llm_answer": answer, "chunks": [f'{c["text"]}' for c in text_metadata_distances]}
+    samples_list.append({
+        "user_input" : entry["question"],
+        "retrieved_contexts" : llm_answer["chunks"],
+        "response" : llm_answer["llm_answer"],
+        "reference" : entry["ground_truth"],
+        "source_article" : entry["source_article"] # but not valid in SingleTurnSample
+        })
+    
+    time.sleep(10)
+
+with open(f"eval/{golden_set_file}_outputs.json", "w") as results_file:
+    json.dump(samples_list, results_file, indent=2)
+
+print(len(samples_list))
+
